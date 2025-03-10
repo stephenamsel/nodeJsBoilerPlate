a757994daf84527a0b9be7d369bb0f6259ab6047
stephenamsel <amselstephen@gmail.com>
2025-03-10 14:08:18 -0400

    cleaned up default-setting for mailer

--- configs/mailer.js
+++ configs/mailer.js
@@ -1,8 +1,10 @@
 const nodemailer = require("nodemailer");
-const { ImapFlow } = require('imapflow'); 
+const { ImapFlow } = require('imapflow');
+const host = process.env("EMAIL_HOST") || "ethereal.email" // SET ENVIRONMENT VARIABLE. Ethereal is for testing
+const hostProtocol = process.env("EMAIL_HOST_PROTOCOL") || "smtp"
 
 let NodeMailerTransporterInput = {
-    host: "smtp.ethereal.email",
+    host: hostProtocol + "." + host,
     port: process.env(EMAIL_PORT),
     secure: false, // Use `true` for port 465, `false` for all other ports
     auth: {
@@ -13,7 +15,7 @@ let NodeMailerTransporterInput = {
 NodeMailerTransporterInput.port ??= 587;
 
 let ReceiverInputs = {
-    host: 'ethereal.email',
+    host: host,
     port: 993,
     secure: true,
     auth: {
